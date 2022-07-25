44d1ba06961565b6764af466a16d385ceda4ae2e
parliz <maris1223@mail.ru>
2022-07-25 14:28:47 +0300

    disappearing notification

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function (Controller) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-       onShowHello : function (oEvent) {
-         let text = oEvent.getSource().getText(); 
-         // show a native JavaScript alert
-          alert("Hello World from " + text);
-       }
-    });
- });
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onShowHello : function () {
+         MessageToast.show("Hello World");
+      }
+   });
+});
