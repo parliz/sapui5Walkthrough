96b6c2d544609f69e84ab2f2f72fe9f2a606fb15
parliz <maris1223@mail.ru>
2022-07-26 15:38:43 +0300

    Step 17: Fragment Callbacks

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -20,7 +20,8 @@ sap.ui.define([
            //if (!this.pDialog) {
                this.pDialog = Fragment.load({
                    id: oView.getId(),
-                   name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                   name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                   controller: this
                }).then(function (oDialog) {
                    // connect dialog to the root view of this component (models, lifecycle)
                    oView.addDependent(oDialog);
@@ -33,6 +34,14 @@ sap.ui.define([
         //    const oDialog = sap.ui.xmlfragment(this.getView().getId(),"sap.ui.demo.walkthrough.view.HelloDialog", this);
         //    this.getView().addDependent(oDialog);
         //    oDialog.open();
-       }
+       },
+		onCloseDialog : function () {
+			// note: We don't need to chain to the pDialog promise, since this event-handler
+			// is only called from within the loaded dialog itself.
+			//this.byId("helloDialog").close();
+            this.pDialog.then(function(oDialog) {
+                oDialog.close();
+            });
+		}
     });
  });
